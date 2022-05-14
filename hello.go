d056bc728d087237cd261a10c49a35af5eb5d4e8
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-14 15:50:43 +0530

    Add comments

--- hello.go
+++ hello.go
@@ -1,9 +1,9 @@
-package main
+package main // Declares the package this code belongs to
 
 import (
-	"fmt"
+	"fmt" // Makes the fmt (format) package available for use
 )
 
-func main() {
-	fmt.Println("Hello, Go!")
+func main() { // Declares a function named main
+	fmt.Println("Hello, Go!") // Prints Hello, GO! to the screen
 }
