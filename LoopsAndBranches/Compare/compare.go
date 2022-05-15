271346b0242c217a83841a4f379a06d60166d808
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-15 17:57:26 +0530

    Add comparing numbers

---
+++ LoopsAndBranches/Compare/compare.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("There is a sign near the entrance that reads 'No Minors'.")
+	var age = 27
+	var minor = age < 18
+	fmt.Printf("At age %v, am I a minor? %v\n", age, minor)
+}
