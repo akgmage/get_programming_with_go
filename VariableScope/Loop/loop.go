f902f20acbb33b6406982fe1a999fb9f5707e034
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-16 23:09:42 +0530

    Add comment

--- VariableScope/Loop/loop.go
+++ VariableScope/Loop/loop.go
@@ -1,3 +1,4 @@
+// The followint listing demonstrates a variant of the for loop that combines initialization, a condition, and a post statement that decrements count.
 package main
 
 import "fmt"
@@ -7,5 +8,5 @@ func main() {
 	for count = 10; count > 0; count-- {
 		fmt.Println(count)
 	}
-	fmt.Println(count)
+	fmt.Println(count) // count remains in scope
 }
