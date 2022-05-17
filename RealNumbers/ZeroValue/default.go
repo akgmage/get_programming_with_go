973e70ed4faa822f302e33fbc54da01223db9d21
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-17 23:43:45 +0530

    Add comment

--- RealNumbers/ZeroValue/default.go
+++ RealNumbers/ZeroValue/default.go
@@ -1,8 +1,11 @@
+// Decalaring a variable without a value
+// In Go each type has a default value, called the zero value
+// The default applies when you declare a variable but don't initialize it with a value
 package main
 
 import "fmt"
 
 func main() {
 	var price float64
-	fmt.Println(price)
+	fmt.Println(price) // Prints 0
 }
