a3f1f43ec8c1464e8f9425d2396d4eb17ccb66b0
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-17 23:39:04 +0530

    Add comment

--- RealNumbers/pi.go
+++ RealNumbers/pi.go
@@ -1,3 +1,4 @@
+// 64-bit vs 32-bit floating-point
 package main
 
 import (
@@ -9,6 +10,6 @@ func main() {
 	var pi64 = math.Pi
 	var pi32 float32 = math.Pi
 
-	fmt.Println(pi64)
-	fmt.Println(pi32)
+	fmt.Println(pi64) // Prints 3.141592653589793
+	fmt.Println(pi32) // Prints 3.1415927
 }
