db9fbe5954b55a387a5860c0d6f13e82b3e23ca2
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-18 22:54:52 +0530

    Add comment

--- RealNumbers/FloatingPointInaccuracies/CompareFloat/compare-float.go
+++ RealNumbers/FloatingPointInaccuracies/CompareFloat/compare-float.go
@@ -1,3 +1,6 @@
+// INstead of comparing floating-point numbers directly,
+// determine the absolute difference between two numbers
+// and then ensure the difference isn't too big
 package main
 
 import (
@@ -8,7 +11,7 @@ import (
 func main() {
 	piggyBank := 0.1
 	piggyBank += 0.2
-	fmt.Println(piggyBank == 0.3)
+	fmt.Println(piggyBank == 0.3) // Prints flase
 
-	fmt.Println(math.Abs(piggyBank-0.3) < 0.0001)
+	fmt.Println(math.Abs(piggyBank-0.3) < 0.0001) // Prints true
 }
