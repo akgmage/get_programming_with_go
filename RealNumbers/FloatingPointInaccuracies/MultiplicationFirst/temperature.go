0ced10d57c60e24e2c603e59478f271c0874eb96
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-18 22:46:56 +0530

    Add comment

--- RealNumbers/FloatingPointInaccuracies/MultiplicationFirst/temperature.go
+++ RealNumbers/FloatingPointInaccuracies/MultiplicationFirst/temperature.go
@@ -1,3 +1,5 @@
+// To minimize rounding errors, we recommend that you perform multiplication before division.
+// The result tends to be more accurate that way.
 package main
 
 import "fmt"
@@ -5,5 +7,8 @@ import "fmt"
 func main() {
 	celsius := 21.0
 	fahrenheit := (celsius * 9.0 / 5.0) + 32.0
-	fmt.Println(fahrenheit, " F")
+	fmt.Println(fahrenheit, " F") // Prints 69.8 F
 }
+
+// What is the best way to avoid rounding errors ?
+// Don't use floating point
