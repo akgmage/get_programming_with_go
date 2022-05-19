f48cf2aaa306fbabc56fb59c380003451c085d56
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-19 23:31:14 +0530

    Add comment

--- WholeNumbers/Bits/bits.go
+++ WholeNumbers/Bits/bits.go
@@ -1,11 +1,13 @@
+// The %b format verb will show you the bits for an integer value.
+// Like other format verbs, %b can be zero padded to a minimum length
 package main
 
 import "fmt"
 
 func main() {
 	var green uint8 = 3
-	fmt.Printf("%08b\n", green)
+	fmt.Printf("%06b\n", green) // Prints 00000011
 
 	green++
-	fmt.Printf("%08b\n", green)
+	fmt.Printf("%06b\n", green) // Prints 00000100
 }
