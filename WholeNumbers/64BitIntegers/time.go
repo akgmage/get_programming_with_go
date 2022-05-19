5410428230528757640a9e4004a82798463d3f25
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-20 00:00:31 +0530

    Add comment

--- WholeNumbers/64BitIntegers/time.go
+++ WholeNumbers/64BitIntegers/time.go
@@ -1,5 +1,6 @@
 // In the year 2038, the number of seconds since January 1, 1970 will exceed two billion, the capacity of int32
-// Thankfully int64 can support dates beyond 2038.
+// Thankfully int64 can support dates beyond 2038. This is a situation where int32 or int simply wont won't do.
+// ONly the int64 and uint64 integer types are able to store numbers well beyond two billions on all platforms.
 // The following uses the Unix function from time package.
 // It accepts tow int64 parameters, corresponding to the nimber of seconds and
 // the number of nanoseconds since January 1, 1970.
