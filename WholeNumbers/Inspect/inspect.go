85cc83c6d9ea73c90cf4fd6f8616604e90eed01e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-19 23:14:35 +0530

    Add comment

--- WholeNumbers/Inspect/inspect.go
+++ WholeNumbers/Inspect/inspect.go
@@ -1,11 +1,16 @@
+// If you're ever curious about which type the Go compiler inferred, the Printf function provides
+// the %T verb to display a variable's type
 package main
 
 import "fmt"
 
 func main() {
 	year := 2018
-	fmt.Printf("Type %T for %v\n", year, year)
+	fmt.Printf("Type %T for %v\n", year, year) // Prints type int for 2018
+
+	// Instead of repeating the variable twice, you can tell Printf to use the first argument [1]
+	// for the second format verb
 
 	days := 365.2425
-	fmt.Printf("Type %T for %v", days, days)
+	fmt.Printf("Type %T for %[1]v", days) // Prints type int for 365.2425
 }
