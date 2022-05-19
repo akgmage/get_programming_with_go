823ff17a91187eeaf53c2676506c54588c86120c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-19 23:24:18 +0530

    Add comment

--- WholeNumbers/IntegersWrapAround/integers-wrap.go
+++ WholeNumbers/IntegersWrapAround/integers-wrap.go
@@ -1,3 +1,4 @@
+// Integers have limited range. When that range is exceeded, integer types in Go, wrap around
 package main
 
 import "fmt"
@@ -6,10 +7,10 @@ func main() {
 
 	var red uint8 = 255
 	red++
-	fmt.Println(red)
+	fmt.Println(red) // Prints 0
 
 	var number int8 = 127
 	number++
-	fmt.Println(number)
+	fmt.Println(number) // Prints -128
 
 }
