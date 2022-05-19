ea82dfd96fd714869a9985159a9d874f1abceb86
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-19 23:36:36 +0530

    Experiment with wrapping behavior of integers

--- WholeNumbers/IntegersWrapAround/integers-wrap.go
+++ WholeNumbers/IntegersWrapAround/integers-wrap.go
@@ -9,8 +9,24 @@ func main() {
 	red++
 	fmt.Println(red) // Prints 0
 
+	red = 255
+	red += 2
+	fmt.Println(red)
+
+	red = 0
+	red--
+	fmt.Println(red)
+
 	var number int8 = 127
 	number++
 	fmt.Println(number) // Prints -128
+
+	number = -128
+	number--
+	fmt.Println(number)
+
+	var green uint16 = 65535
+	green++
+	fmt.Println(green)
 
 }
