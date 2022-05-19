a974790077fc511d44ab9ec2b348d5341f53e4ff
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-19 23:42:03 +0530

    Add comment

--- WholeNumbers/BitsWhenIntegerWrap/bits-wrap.go
+++ WholeNumbers/BitsWhenIntegerWrap/bits-wrap.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	var blue uint8 = 255
-	fmt.Printf("%08b\n", blue)
+	fmt.Printf("%08b\n", blue) // Prints 11111111
 
 	blue++
-	fmt.Printf("%08b\n", blue)
+	fmt.Printf("%08b\n", blue) // Prints 00000000
 }
