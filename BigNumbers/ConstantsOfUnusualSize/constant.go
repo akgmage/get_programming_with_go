164a078d432e8d6b8e0c391a6baa377edb2319ba
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-20 23:40:47 +0530

    Demonstrate literal & Add comment

--- BigNumbers/ConstantsOfUnusualSize/constant.go
+++ BigNumbers/ConstantsOfUnusualSize/constant.go
@@ -3,12 +3,15 @@
 // For variables, Go uses type inference to determine the type, and in the case of 24 quintillion, overflows int type.
 // Constants are different. rather than infer a type, constants can be untyped.
 // const distance = 24000000000000000000 doesn't cause an overflow error
-
+// Constant are declared with const keyword, but every literal value in your program is a constant too.
+// This means unusually sized numbers can be used directly.
 package main
 
 import "fmt"
 
 func main() {
+
+	fmt.Println("Andromeda Galaxy is", 24000000000000000000/299792/86400, "light days away") // // Prints Andromeda Galaxy is 926568346 light days away.
 
 	const distance = 24000000000000000000
 	const lightSpeed = 299792
