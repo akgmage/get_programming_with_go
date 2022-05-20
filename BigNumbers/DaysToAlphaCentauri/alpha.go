e3eee2eee4e0189bd12988dabdac0bd315cb6340
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-20 23:14:59 +0530

    Add comment

--- BigNumbers/DaysToAlphaCentauri/alpha.go
+++ BigNumbers/DaysToAlphaCentauri/alpha.go
@@ -1,3 +1,4 @@
+// Rather than painstakingly typing every zero, you can write such numbers ig Go with exponent
 package main
 
 import "fmt"
@@ -7,9 +8,9 @@ func main() {
 	const secondsPerDay = 86400
 
 	var distance int64 = 41.3e12
-	fmt.Println("Alpha Centauri is", distance, "km away.")
+	fmt.Println("Alpha Centauri is", distance, "km away.") // Prints Alpha Centauri is 41300000000000 km away.
 
 	days := distance / lightSpeed / secondsPerDay
-	fmt.Println("That is", days, "days of travel at light speed.")
+	fmt.Println("That is", days, "days of travel at light speed.") // Prints That is 1594 days of travel at light speed.
 
 }
