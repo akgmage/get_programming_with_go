15f32702473ebd6f09e24c9f07e79678c3d90965
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-21 22:56:00 +0530

    Add comment

--- Unit-2-Types/MultiLingualText/MultipleLineRawStrings/raw-lines.go
+++ Unit-2-Types/MultiLingualText/MultipleLineRawStrings/raw-lines.go
@@ -1,3 +1,5 @@
+// Unlike conventional string literals, raw string literals can span multiple lines of source code
+// The following will produce output, including the tabs used for indentation
 package main
 
 import "fmt"
