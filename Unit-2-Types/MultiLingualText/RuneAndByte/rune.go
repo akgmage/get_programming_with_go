d1c741078a29212bd79b09bc8b5bf1f66fed8cda
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-21 23:31:15 +0530

    Add comment

--- Unit-2-Types/MultiLingualText/RuneAndByte/rune.go
+++ Unit-2-Types/MultiLingualText/RuneAndByte/rune.go
@@ -10,4 +10,11 @@ func main() {
 	var bang byte = 33
 	fmt.Printf("%v %v %v %v\n", pi, alpha, omega, bang) // Prints 960 940 969 33
 	fmt.Printf("%c %c %c %c\n", pi, alpha, omega, bang) // Prints π ά ω !
+
+	// Rather than memorize Unicode code points, Go provides a character literal,
+	// Just enclose a character in a single quotes 'A'. If no type is specified.
+	// Go will infer a rune. So following three lines are equivalent.
+	// grade := 'A'
+	// var grade = 'A'
+	// var grade rune = 'A'
 }
