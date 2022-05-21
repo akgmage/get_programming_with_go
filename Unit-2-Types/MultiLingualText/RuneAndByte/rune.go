890b177f1a621d7229cdb5260c3788e7dcbfc7d3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-21 23:27:06 +0530

    Add comment

--- Unit-2-Types/MultiLingualText/RuneAndByte/rune.go
+++ Unit-2-Types/MultiLingualText/RuneAndByte/rune.go
@@ -1,3 +1,4 @@
+// To represent a single Unicode code point, Go provides rune, which is an alias for the int32 type.
 package main
 
 import "fmt"
@@ -7,6 +8,6 @@ func main() {
 	var alpha rune = 940
 	var omega rune = 969
 	var bang byte = 33
-	fmt.Printf("%v %v %v %v\n", pi, alpha, omega, bang)
-	fmt.Printf("%c %c %c %c\n", pi, alpha, omega, bang)
+	fmt.Printf("%v %v %v %v\n", pi, alpha, omega, bang) // Prints 960 940 969 33
+	fmt.Printf("%c %c %c %c\n", pi, alpha, omega, bang) // Prints π ά ω !
 }
