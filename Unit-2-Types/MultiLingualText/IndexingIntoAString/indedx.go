d6b9eea5229a89d913edb2d72251293cba22ab21
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-21 23:39:23 +0530

    Add comment

--- Unit-2-Types/MultiLingualText/IndexingIntoAString/indedx.go
+++ Unit-2-Types/MultiLingualText/IndexingIntoAString/indedx.go
@@ -1,3 +1,7 @@
+// Your program can access individual characters, but it can't alter the characters of a string.
+// The following listing uses square brackets [] to specify an index into a string which accesses
+// a single byte (ASCII character). The index starts from zero
+
 package main
 
 import "fmt"
@@ -5,9 +9,9 @@ import "fmt"
 func main() {
 	peace := "shalom"
 	peace = "salam"
-	fmt.Println(peace)
+	fmt.Println(peace) // Prints salam
 
 	message := "shalom"
 	c := message[5]
-	fmt.Printf("%c\n", c)
+	fmt.Printf("%c\n", c) // Prints m
 }
