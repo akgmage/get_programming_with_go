482faa49f18023dd1770a154c6afe0ae79c6050e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-21 23:14:02 +0530

    Add comment

--- Unit-2-Types/MultiLingualText/StringType/raw-type.go
+++ Unit-2-Types/MultiLingualText/StringType/raw-type.go
@@ -1,8 +1,9 @@
+// Literal strings and raw strings both result in strings
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Printf("%v is a %[1]T\n", "literal string", "asd")
-	fmt.Printf("%v is a %[1]T\n", "raw string literal")
+	fmt.Printf("%v is a %[1]T\n", "literal string", "asd") // Prints literal string is a string
+	fmt.Printf("%v is a %[1]T\n", `raw string literal`)    // Prints raw string literal is a string
 }
