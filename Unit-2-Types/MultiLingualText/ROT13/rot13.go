aaa8f2334050c11f5bc6dc3c9c36b462bfaf098c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-23 23:13:45 +0530

    Add comment

--- Unit-2-Types/MultiLingualText/ROT13/rot13.go
+++ Unit-2-Types/MultiLingualText/ROT13/rot13.go
@@ -1,3 +1,4 @@
+// The following listing will decipher a message from space
 package main
 
 import "fmt"
@@ -5,14 +6,14 @@ import "fmt"
 func main() {
 	message := "uv vagreangvbany fcnpr fngvba"
 
-	for i := 0; i < len(message); i++ {
+	for i := 0; i < len(message); i++ { // Iterates through each ASCII character
 		c := message[i]
-		if c >= 'a' && c <= 'z' {
+		if c >= 'a' && c <= 'z' { // Leaves spaces and punctuation as they are
 			c = c + 13
 			if c > 'z' {
 				c = c - 26
 			}
 		}
-		fmt.Printf("%c", c)
+		fmt.Printf("%c", c) // Prints hi international space sation
 	}
 }
