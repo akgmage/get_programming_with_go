0f9d4ea2b1253e24631c417f053e86e30362399f
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-23 23:25:31 +0530

    Add comment

--- Unit-2-Types/MultiLingualText/DecodingStringIntoRunes/spanish.go
+++ Unit-2-Types/MultiLingualText/DecodingStringIntoRunes/spanish.go
@@ -1,3 +1,5 @@
+// The first step to supporting other languages is to decode characters to the rune type before manipulating them.
+// Fortunately, Go has functions and language features for decoding UTF-8 encoded strings
 package main
 
 import (
@@ -8,10 +10,10 @@ import (
 func main() {
 	question := "¿cómo estás?"
 
-	fmt.Println(len(question), "bytes")
-	fmt.Println(utf8.RuneCountInString(question), "runes")
+	fmt.Println(len(question), "bytes")                    // Prints 15 bytes
+	fmt.Println(utf8.RuneCountInString(question), "runes") // Prints 12 runes
 
 	c, size := utf8.DecodeLastRuneInString(question)
-	fmt.Printf("First rune: %c %v bytes", c, size)
+	fmt.Printf("First rune: %c %v bytes", c, size) // Prints First rune: ? 1 bytes
 
 }
