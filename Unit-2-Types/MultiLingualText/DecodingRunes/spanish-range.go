2c6eadef16910e76ee99b3b0b6a6be81a1cb92ac
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-23 23:46:09 +0530

    Use _ to ignore the index && Add comment

--- Unit-2-Types/MultiLingualText/DecodingRunes/spanish-range.go
+++ Unit-2-Types/MultiLingualText/DecodingRunes/spanish-range.go
@@ -1,3 +1,5 @@
+// The Go language provides the range keyword to iterate over a variety of collections.
+// It can also decode UTF-8 encoded strings
 package main
 
 import "fmt"
@@ -8,4 +10,23 @@ func main() {
 	for i, c := range question {
 		fmt.Printf("%v %c\n", i, c)
 	}
+	// If you dont need the index, the blank (an undersocre) allows you to ignore it
+	for _, c := range question {
+		fmt.Printf("%c", c) // Prints ¿cómo estás?
+	}
+	// Prints
+	/*
+		0 ¿
+		2 c
+		3 ó
+		5 m
+		6 o
+		7
+		8 e
+		9 s
+		10 t
+		11 á
+		13 s
+		14 ?
+	*/
 }
