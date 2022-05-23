75efbeb2d9f30e96564c1d1be321772b66c55fa6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-23 23:30:36 +0530

    Change DecodeLastRuneInString to DecodeRuneInString & Add comment

--- Unit-2-Types/MultiLingualText/DecodingStringIntoRunes/spanish.go
+++ Unit-2-Types/MultiLingualText/DecodingStringIntoRunes/spanish.go
@@ -1,5 +1,7 @@
 // The first step to supporting other languages is to decode characters to the rune type before manipulating them.
 // Fortunately, Go has functions and language features for decoding UTF-8 encoded strings
+// The utf package provides functions to determine the length of string in runes rather than bytes
+// and to decode the first character of a string
 package main
 
 import (
@@ -13,7 +15,7 @@ func main() {
 	fmt.Println(len(question), "bytes")                    // Prints 15 bytes
 	fmt.Println(utf8.RuneCountInString(question), "runes") // Prints 12 runes
 
-	c, size := utf8.DecodeLastRuneInString(question)
+	c, size := utf8.DecodeRuneInString(question)
 	fmt.Printf("First rune: %c %v bytes", c, size) // Prints First rune: ? 1 bytes
 
 }
