7309fcbef7af3c4f57d6c3cb0873597425099ac3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-23 23:22:24 +0530

    Demonstrate utf8

---
+++ Unit-2-Types/MultiLingualText/DecodingStringIntoRunes/spanish.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	question := "¿cómo estás?"
+
+	fmt.Println(len(question), "bytes")
+	fmt.Println(utf8.RuneCountInString(question), "runes")
+
+	c, size := utf8.DecodeLastRuneInString(question)
+	fmt.Printf("First rune: %c %v bytes", c, size)
+
+}
