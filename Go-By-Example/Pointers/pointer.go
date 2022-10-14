33dd6cadf54330c490987dffd88d2a5bad4f002a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-14 22:50:31 +0530

    add comment

--- Go-By-Example/Pointers/pointer.go
+++ Go-By-Example/Pointers/pointer.go
@@ -5,7 +5,10 @@ import "fmt"
 func zeroval(ival int) {
 	ival = 0
 }
+/// takes an int pointer [iptr]
 func zeroptr(iptr *int) {
+	// assigning a value to a dereferenced pointer changes the value 
+	// at the referenced address.
 	*iptr = 0
 }
 
@@ -16,6 +19,7 @@ func main() {
 	fmt.Println("zeroval", i)
 	zeroptr(&i)
 	fmt.Println("Pointer", i)
+	// &i gives the memory address of i
 	fmt.Println("Pointer address", &i)
 
 }
