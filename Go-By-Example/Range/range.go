7e3eb1bfbce2a3403b7b772dc308760603395179
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-15 22:37:04 +0530

    iterate over kv pairs

--- Go-By-Example/Range/range.go
+++ Go-By-Example/Range/range.go
@@ -12,4 +12,15 @@ func main() {
 		sum += num
 	}
 	fmt.Println(sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index", i)
+		}
+	}
+
+	kvs := map[string]string{"a": "apple", "b":"banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
 }
