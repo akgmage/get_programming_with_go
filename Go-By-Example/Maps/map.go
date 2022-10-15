7ebbb6f09b7f594b496c71f6982d9d10e0c0d34b
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-15 22:22:29 +0530

    add comment

--- Go-By-Example/Maps/map.go
+++ Go-By-Example/Maps/map.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	// create an empty map, use the builtin make: make(map[key-type]val-type)
 	m := make(map[string]int)
-
+	// Set key/value pairs using typical name[key] = val syntax
 	m["item1"] = 7
 	m["item2"] = 9
 
 	fmt.Println("map:", m)
-
+	// len returns the number of key/value pairs when called on a map
+	fmt.Println("len:", len(m))
 	x := m["item1"]
 	fmt.Println("x:",x)
 	delete(m, "item2")
