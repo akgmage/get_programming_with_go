a472690abff68be3e10ebfa40e7a30f3333e4777
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-15 22:28:12 +0530

    add comment

--- Go-By-Example/Range/range.go
+++ Go-By-Example/Range/range.go
@@ -1,3 +1,4 @@
+// range iterates over elements in a variety of data structures
 package main
 
 import "fmt"
@@ -5,6 +6,8 @@ import "fmt"
 func main() {
 	nums := []int{2, 3, 4}
 	sum := 0
+	// range on arrays and slices provides both the index and value for each entry
+	// ignore index with blank identifier _
 	for _, num := range nums {
 		sum += num
 	}
