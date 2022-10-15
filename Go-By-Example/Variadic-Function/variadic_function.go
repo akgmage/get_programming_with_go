bdd772034f7491a001e62669076700326f9a7584
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-15 22:46:33 +0530

    add comment

--- Go-By-Example/Variadic-Function/variadic_function.go
+++ Go-By-Example/Variadic-Function/variadic_function.go
@@ -1,7 +1,9 @@
+/// Variadic functions can be called with any number of trailing arguments.
+/// For example, fmt.Println is a common variadic function
 package main
 
 import "fmt"
-
+/// function sum that will take an arbitrary number of ints as arguments
 func sum(nums ...int) {
 	fmt.Print(nums, " ")
 	total := 0
