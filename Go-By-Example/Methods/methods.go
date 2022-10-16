c344e31e34b405154a6724fe84759d0344249cca
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-16 21:24:43 +0530

    use a pointer receiver type to avoid copying on method calls

--- Go-By-Example/Methods/methods.go
+++ Go-By-Example/Methods/methods.go
@@ -19,4 +19,10 @@ func main() {
 	r := rect{width: 10, height: 5}
 	fmt.Println("area: ", r.area())
 	fmt.Println("perim: ", r.perim())
+
+	// use a pointer receiver type to avoid copying on method calls or to allow the method to mutate the receiving struct
+	rp := &r
+	fmt.Println("area: ", rp.area())
+	fmt.Println("perim: ", rp.perim())
+
 }
