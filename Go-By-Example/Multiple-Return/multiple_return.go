c8584d6d81410b193da66230c872672d96116797
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-16 21:57:17 +0530

    add comment

--- Go-By-Example/Multiple-Return/multiple_return.go
+++ Go-By-Example/Multiple-Return/multiple_return.go
@@ -1,3 +1,5 @@
+/// Go has built-in support for multiple return values. This feature is used often in idiomatic Go, 
+/// for example to return both result and error values from a function
 package main
 
 import "fmt"
