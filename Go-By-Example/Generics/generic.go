0a96ebc48d72083769455b7f5253dd696a60d43c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-17 23:08:17 +0530

    add func to push item in ll

--- Go-By-Example/Generics/generic.go
+++ Go-By-Example/Generics/generic.go
@@ -16,3 +16,13 @@ type element[T any] struct {
 	next *element[T]
 	val T
 }
+
+func (lst *List[T]) Push(v, T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
