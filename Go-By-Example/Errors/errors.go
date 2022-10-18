2c4841667c2db3a6b104a7ed0afebf8069353c3c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-18 22:55:57 +0530

    test out error returning function

--- Go-By-Example/Errors/errors.go
+++ Go-By-Example/Errors/errors.go
@@ -23,3 +23,21 @@ type argError struct {
 func (e *argError) Error() string {
 	return fmt.Sprintf("%d - %s", e.arg, e.prob)
 }
+
+func f2(arg int) (int, error) {
+	if arg == 42 {
+		return -1, &argError{arg, "can't work with it"}
+	}
+	return arg + 3, nil
+}
+
+func main() {
+	// test out error-returning functions
+	for _, i := range []int{6, 43} {
+		if r, e := f1(i); e != nil {
+			fmt.Println("f1 failed:", e)
+		} else {
+			fmt.Println("f1 worked:", r)
+		}
+	}
+}
