f1e5c145502cd853367d493455638230628d7840
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-18 22:47:15 +0530

    add f1 func and argError struct

---
+++ Go-By-Example/Errors/errors.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+func f1(arg int) (int, error) {
+	if arg == 42 {
+		return -1, errors.New("can't work with 42")
+	}
+	return arg + 3, nil
+}
+
+type argError struct {
+	arg int
+	prob string
+}
+
