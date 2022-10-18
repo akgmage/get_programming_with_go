4ee548a1968342d4150d92f1db42a18262eef67c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-18 22:48:14 +0530

    add comments

--- Go-By-Example/Errors/errors.go
+++ Go-By-Example/Errors/errors.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"errors"
-	"fmt"
 )
+
+// By convention, errors are the last return value and have type error, a built-in interface
 func f1(arg int) (int, error) {
 	if arg == 42 {
+		// errors.New constructs a basic error value with the given error message
 		return -1, errors.New("can't work with 42")
 	}
+	// A nil value in the error position indicates that there was no error
 	return arg + 3, nil
 }
 
