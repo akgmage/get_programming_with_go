b7c5aa59a2158814fa4dc08549f93b433a4a6a4f
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-22 22:04:15 +0530

    add comment

--- Go-By-Example/Timeouts/timeouts.go
+++ Go-By-Example/Timeouts/timeouts.go
@@ -9,6 +9,9 @@ import (
 
 func main() {
 	c1 := make(chan string, 1)
+	// suppose we’re executing an external call that returns its result on a channel c1 after 2s.
+	// Note that the channel is buffered, so the send in the goroutine is nonblocking.
+	// This is a common pattern to prevent goroutine leaks in case the channel is never read
 	go func() {
 		time.Sleep(2 * time.Second)
 		c1 <- "result 1"
