9bdfc6d6aa2c364e0a45ad64dbe5ce14a54ca0a2
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-23 22:12:43 +0530

    add comment

--- Go-By-Example/Range Over Channels/roc.go
+++ Go-By-Example/Range Over Channels/roc.go
@@ -1,8 +1,10 @@
+// Iterate over values received from a channel
 package main
 
 import "fmt"
 
 func main() {
+	// iterate over 2 values in the queue channel
 	queue := make(chan string, 2)
 	queue <- "one"
 	queue <- "two"
