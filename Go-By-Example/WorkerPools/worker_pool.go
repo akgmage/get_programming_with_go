87ea5e1541ffea811fb109b3be3d21e66eb14260
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-01 22:53:07 +0530

    add comment

--- Go-By-Example/WorkerPools/worker_pool.go
+++ Go-By-Example/WorkerPools/worker_pool.go
@@ -1,10 +1,13 @@
+// Implement a worker pool using goroutines and channels.
 package main
 
 import (
 	"fmt"
 	"time"
 )
-
+// Here’s the worker, of which we’ll run several concurrent 
+// instances. These workers will receive work on the jobs channel and send the corresponding results on results. 
+// We’ll sleep a second per job to simulate an expensive task.
 func worker(id int, jobs <-chan int, result chan<- int) {
 	for j:= range jobs {
 		fmt.Println("worker", id, "started job", j)
