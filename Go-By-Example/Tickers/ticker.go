3d35b00002acc9dd702de2eda94b4e3bacfa0602
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-01 22:36:15 +0530

    fire a goroutine

--- Go-By-Example/Tickers/ticker.go
+++ Go-By-Example/Tickers/ticker.go
@@ -7,4 +7,15 @@ import (
 func main() {
 	ticker := time.NewTicker(500 * time.Millisecond)
 	done := make(chan bool)
+
+	go func() {
+		for  {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				fmr.Println("Tick at", t)
+			}
+		}
+	}()
 }
