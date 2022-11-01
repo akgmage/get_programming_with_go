4df3bbfabb687e2554aeab503d5ab49b1f89dc9d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-01 22:39:44 +0530

    add comment

--- Go-By-Example/Tickers/ticker.go
+++ Go-By-Example/Tickers/ticker.go
@@ -7,6 +7,11 @@ import (
 )
 
 func main() {
+	/*
+		Tickers use a similar mechanism to timers: 
+		a channel that is sent values. Here we’ll use the select builtin 
+		on the channel to await the values as they arrive every 500ms.
+	*/
 	ticker := time.NewTicker(500 * time.Millisecond)
 	done := make(chan bool)
 
