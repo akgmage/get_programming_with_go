134f028faa84bd6ed87dc51fecd34e434769cb8e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-01 22:51:54 +0530

    receive work on the jobs channel and send the corresponding results on results

--- Go-By-Example/WorkerPools/worker_pool.go
+++ Go-By-Example/WorkerPools/worker_pool.go
@@ -9,5 +9,7 @@ func worker(id int, jobs <-chan int, result chan<- int) {
 	for j:= range jobs {
 		fmt.Println("worker", id, "started job", j)
 		time.Sleep(time.Second)
+		fmt.Println("worker", id, "finishedjob", j)
+		result <- j * 2
 	}
 }
