86e8fe3f76d5d644f14516b5fc669964ed441474
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-02 23:06:03 +0530

    add comment

--- Go-By-Example/WaitGroups/wait_group.go
+++ Go-By-Example/WaitGroups/wait_group.go
@@ -15,12 +15,16 @@ func worker(id int) {
 }
 
 func main() {
+	// This WaitGroup is used to wait for all the goroutines 
+	// launched here to finish
 	var wg sync.WaitGroup
-
+	// Launch several goroutines and increment the WaitGroup counter for each.
 	for i := 1; i <= 5; i++ {
 		wg.Add(1)
-		i := i
-
+		i := i // Avoid re-use of the same i value in each goroutine closure
+		// Wrap the worker call in a closure that makes sure to tell 
+		// the WaitGroup that this worker is done. This way the worker itself 
+		// does not have to be aware of the concurrency primitives involved in its execution.
 		go func() {
 			defer wg.Done()
 			worker(i)
