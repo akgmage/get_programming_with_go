cda3bcbf7e54f027edc5dccd0166b9e3ceb82a52
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-02 22:59:27 +0530

    launch several goroutines and increment the waitgroup counter foreach

--- Go-By-Example/WaitGroups/wait_group.go
+++ Go-By-Example/WaitGroups/wait_group.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -9,4 +10,20 @@ func worker(id int) {
 	fmt.Printf("Worker %d starting\n", id)
 	time.Sleep(time.Second)
 	fmt.Printf("Worker %d done\n", id)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		i := i
+
+		go func() {
+			defer wg.Done()
+			worker(i)
+		}()
+	}
+	
+
 }
