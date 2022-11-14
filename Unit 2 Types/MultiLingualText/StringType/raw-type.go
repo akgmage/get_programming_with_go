bb3a4611fd21ae0e7520f6447644b9a0aaa9e1f1
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-14 22:45:04 +0530

    update description and program

--- Unit 2 Types/MultiLingualText/StringType/raw-type.go
+++ Unit 2 Types/MultiLingualText/StringType/raw-type.go
@@ -4,6 +4,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%v is a %[1]T\n", "literal string", "asd") // Prints literal string is a string
+	// %T a Go-syntax representation of the type of the value
+	// [SOME_NUMBER] refers to value in SOME_NUMBER position 
+	fmt.Printf("%v is a %[1]T\n", "literal string", 2) // Prints literal string is a string
 	fmt.Printf("%v is a %[1]T\n", `raw string literal`)    // Prints raw string literal is a string
 }
