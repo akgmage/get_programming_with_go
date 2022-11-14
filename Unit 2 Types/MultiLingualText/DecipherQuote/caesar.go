56da59dedda5dcda8f5c257df45460f8e2857d9d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-11-14 22:26:24 +0530

    fix typo leading to incorrect answer in case of letter a, b and c

--- Unit 2 Types/MultiLingualText/DecipherQuote/caesar.go
+++ Unit 2 Types/MultiLingualText/DecipherQuote/caesar.go
@@ -5,14 +5,13 @@ package main
 import "fmt"
 
 func main() {
-	message := "L fdph, L vdz, L frqtxhuhg"
+	message := "L fdph, L vdz, L frqtxhuhg abc"
 
 	for i := 0; i < len(message); i++ {
 		c := message[i]
-		fmt.Println(c)
 		if c >= 'A' && c <= 'z' {
 			c = c - 3 // shift 3 letters backwards
-			if c < 'a' && c > 'z' {
+			if c < 'a' && c > 'Z' {
 				c = c + 26
 			}
 		}
